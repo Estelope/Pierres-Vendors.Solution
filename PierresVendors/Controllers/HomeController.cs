b8b9baf7cd398f982895b5b03f4b62e1c3f5df37
Estelope <elopez14567@gmail.com>
2023-09-21 19:29:11 -0600

    add home route,  display home ui, start first model

---
+++ PierresVendors/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresVendors.Models;
+
+namespace PierresVendors.Controllers
+{
+  public class HomeController : Controller  
+  {
+
+        [HttpGet("/")] 
+      public ActionResult Index() 
+      {
+        return View();
+      }
+  }
+}
