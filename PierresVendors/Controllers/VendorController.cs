c3bbfeb27e499f98468d62d3f87e574a8ddd5220
Estelope <elopez14567@gmail.com>
2023-09-21 21:26:17 -0600

    add two methods to Vendor, add Vendor routes

--- PierresVendors/Controllers/VendorController.cs
+++ PierresVendors/Controllers/VendorController.cs
@@ -27,5 +27,29 @@ namespace PierresVendors.Controllers
     Vendor newVendor = new Vendor(vendorName, vendorDescription);
     return RedirectToAction("Index");
   }
+
+    [HttpGet("/vendors/{id}")]
+  public ActionResult Show(int id)
+  {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Vendor selectedVendor = Vendor.Find(id);
+    List<Order> vendorOrders = selectedVendor.Orders;
+    model.Add("vendor", selectedVendor);
+    model.Add("orders", vendorOrders);
+    return View(model);
+  }
+
+  [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string vendorDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(vendorDescription);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendors", foundVendor);
+      return View("Show", model);
+    }
   }
 }
--- PierresVendors/Models/Vendors.cs
+++ PierresVendors/Models/Vendors.cs
@@ -28,6 +28,14 @@ namespace PierresVendors.Models
     {
       return _instances;
     }
+            public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+  }
 
   }
-}
